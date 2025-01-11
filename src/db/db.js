d30db5b0ba3ba31c7448135cfbd3d704b277dc1a
Utkarsh Sharma <utkarshdev2411@gmail.com>
2025-01-11 16:39:08 +0530

    Enhance database connection error handling and improve connection options

--- src/db/db.js
+++ src/db/db.js
@@ -1,14 +1,22 @@
-const mongoose=require('mongoose')
-const config=require('../config/default')
+const mongoose = require('mongoose');
+const config = require('../config/default');
 
-const connectDB=()=>{
-    mongoose.connect(config.database.mongoURI)
-    .then(()=>{
-        console.log('Database connected!');
+const connectDB = () => {
+  mongoose
+    .connect(config.database.mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000,
+      socketTimeoutMS: 45000
     })
-    .catch((e)=>{
-        console.log('Error connecting the database');
-        console.log(e);
+    .then(() => {
+      console.log('Database connected!');
     })
-}
-module.exports=connectDB;
+    .catch((e) => {
+      console.error('Error connecting to the database:');
+      console.error(e.message || e);
+      process.exit(1);
+    });
+};
+
+module.exports = connectDB;
